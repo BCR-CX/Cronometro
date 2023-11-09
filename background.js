50d61950ec99ebba935c3fd4816df57f852a365a
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-11-09 13:40:08 -0300

    Release 3.0.0

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ let segundos_BACKGROUND
 let minutos_BACKGROUND
 let switch_Começar_BACKGROUND
 let intervalo_BACKGROUND;
-
+let prevent_BACKGROUND
 
 chrome.tabs.onActivated.addListener((tab) => {
     console.log(tab)
@@ -36,6 +36,9 @@ function Começar_Background(i, j, switch_Começar) {
         chrome.storage.local.set({ segundos_BACKGROUND: i, minutos_BACKGROUND: j, switch_Começar_BACKGROUND: switch_Começar })
         return switch_Começar;
     } else {
+        prevent_BACKGROUND = setInterval(() => {
+            fetch("./manifest.json").then((response) => {console.warn(response)})
+        }, 15000)
         intervalo_BACKGROUND = setInterval(function () {
             i++;
             if (i > 59) {
@@ -81,8 +84,10 @@ chrome.runtime.onConnect.addListener(function () {
     }
     else if (switch_Começar_BACKGROUND == Boolean(false)) {
         clearInterval(intervalo_BACKGROUND);
+        clearInterval(prevent_BACKGROUND);
         (async () => {
             const response = await chrome.runtime.sendMessage({greeting: "StopwatchContinue", minutos_BACKGROUND: minutos_BACKGROUND, segundos_BACKGROUND: segundos_BACKGROUND});
         })();
+
     }
 })
